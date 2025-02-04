254cda90a210842a9f86af93ed50a96d31658644
teiv273 <nguyenviet2k72k3@gmail.com>
2025-02-04 11:08:38 +0700

    2/4/25

--- frontend/memoapp/lib/dialogs/delete_memo_dialog.dart
+++ frontend/memoapp/lib/dialogs/delete_memo_dialog.dart
@@ -9,5 +9,38 @@ class DeleteMemoDialog extends StatefulWidget {
     required this.index,
     required this.scrollToBottom,
     super.key,
-  })
+  });
+
+  @override
+  State<DeleteMemoDialog> createState() => _DeleteMemoDialogState();
+}
+
+class _DeleteMemoDialogState extends State<DeleteMemoDialog> {
+  RxString status = 'delete-memo'.obs;
+  var memoController = TextEditingController();
+
+  Widget deleteMemoWidget() {
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const Text(
+            'Delete this memo?',
+          ),
+          const SizedBox(height: 20),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              ElevatedButton(onPressed: () {
+                status.value = 'deleting-memo';
+              }, child: const Text('Delete')),
+
+              const SizedBox(width: 20),
+              ElevatedButton(onPressed: onPressed, child: const Te)
+            ],
+          )
+        ],
+      ),
+    )
+  }
 }
